e9ebae1ad53b5792452e9d684cae8d61ec9fa912
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-06-04 22:10:47 +0500

    Refactor RootLayout component: wrap layout in Providers for improved context management and add Toaster for notifications, enhancing user experience.

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Toaster } from "@/components/ui/sonner";
+import Providers from "../hooks/providers";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,22 +25,25 @@ export default function RootLayout({ children }) {
     >
       <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className}`}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {/* Header */}
-            <Header />
-            <main>{children}</main>
-            {/* Footer */}
-            <footer className="bg-muted/80 py-12">
-              <div className="container mx-auto px-4  text-center">
-                <p className=" text-gray-200">Made with ❤️ by Zameer.Dev</p>
-              </div>
-            </footer>
-          </ThemeProvider>
+          <Providers>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {/* Header */}
+              <Header />
+              <main className="min-h-screen">{children}</main>
+              <Toaster />
+              {/* Footer */}
+              <footer className="bg-muted/80 py-12">
+                <div className="container mx-auto px-4  text-center">
+                  <p className=" text-gray-200">Made with ❤️ by Zameer.Dev</p>
+                </div>
+              </footer>
+            </ThemeProvider>
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
